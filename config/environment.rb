515a2b76fd6599ec6487427b9adb3b10190190b3
Emily Paige Brown <brown.emily.paige@gmail.com>
2018-01-18 18:10:05 +0000

    fixed environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.action_mailer.default_url_options = { :host => 'https://blocipedia-emilypbrown.c9users.io/' }
-
 ActionMailer::Base.smtp_settings = {
   :user_name => 'app85931423@heroku.com',
   :password => '3q2tmkvr3861',
