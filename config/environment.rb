81958a34890bd6a04de2c9f76fbd0b4d3b4c4762
Emily Paige Brown <brown.emily.paige@gmail.com>
2018-01-18 17:58:14 +0000

    Email still not working fully

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ config.action_mailer.default_url_options = { :host => 'https://blocipedia-emilyp
 ActionMailer::Base.smtp_settings = {
   :user_name => 'app85931423@heroku.com',
   :password => '3q2tmkvr3861',
-  :domain => 'yourdomain.com',
+  :domain => 'https://sleepy-shelf-21352.herokuapp.com/',
 #   blocipedia-emilypbrown.c9users.io ?
   :address => 'smtp.sendgrid.net',
   :port => 587,
