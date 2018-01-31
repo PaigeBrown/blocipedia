3c5300887ba82bdeece36daf2f76b81554036d52
Emily Paige Brown <brown.emily.paige@gmail.com>
2018-01-31 15:24:08 +0000

    upgrade done!

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -2,39 +2,33 @@ class ChargesController < ApplicationController
     
     def create
         @amount = 10_00,
-   # Creates a Stripe Customer object, for associating
-   # with the charge
+   
        customer = Stripe::Customer.create(
-         email: current_user.email,
-         card: params[:stripeToken]
-       )
+            email: current_user.email,
+            card: params[:stripeToken]
+        )
      
-       # Where the real magic happens
        charge = Stripe::Charge.create(
-         customer: customer.id, # Note -- this is NOT the user_id in your app
-        #  amount: Amount.default,
-         amount: @amount,
-         description: "BigMoney Membership - #{current_user.email}",
-         currency: 'usd'
-       )
+            customer: customer.id, 
+            amount: 1000,
+            description: "BigMoney Membership - #{current_user.email}",
+            currency: 'usd'
+        )
      
        flash[:notice] = "Thanks for all the money, #{current_user.email}! Feel free to pay me again."
-       redirect_to user_path(current_user) # or wherever
-     
-       # Stripe will send back CardErrors, with friendly messages
-       # when something goes wrong.
-       # This `rescue block` catches and displays those errors.
-       rescue Stripe::CardError => e
-         flash[:alert] = e.message
-         redirect_to new_charge_path
+       redirect_to user_index_path(current_user) 
+       
+        rescue Stripe::CardError => e
+        flash[:alert] = e.message
+        redirect_to new_charge_path
     end
     
     def new
         @amount = 10_00,
-       @stripe_btn_data = {
-         key: "#{ Rails.configuration.stripe[:publishable_key] }",
-         description: "BigMoney Membership - #{current_user.email}",
-         amount: @amount,
-       }
+        @stripe_btn_data = {
+            key: "pk_test_fhhBFwNfJ2ZUJv74iCFQxKRd",
+            description: "BigMoney Membership - #{current_user.email}",
+            amount: @amount
+        }
     end
 end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,11 +1,14 @@
+require "stripe"
+
 Rails.configuration.stripe = {
-   publishable_key: 'STRIPE_PUBLISHABLE_KEYs',
+   publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
    secret_key: ENV['STRIPE_SECRET_KEY']
  }
  
- Stripe.publishable_key = Rails.configuration.stripe[:publishable_key]
- # Set our app-stored secret key with Stripe
+ 
+ 
  Stripe.api_key = Rails.configuration.stripe[:secret_key]
+ 
 
 
 
